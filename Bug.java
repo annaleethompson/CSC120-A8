db388c63d59ac8f78a00d281c9e125cf85d1084c
annaleethompson <acthompson@smith.edu>
2023-04-09 13:34:37 -0400

    Day2

--- Bug.java
+++ Bug.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Bug {//implements Contract {
+public class Bug { //implements Contract {
     
     String name;
     String type;
@@ -8,51 +8,203 @@ public class Bug {//implements Contract {
     int x_position;
     int y_position;
     int size;
-    int alertness;
+    int energy;
 
-    public Bug(String name, String type, int size, int alertness) {
+    public Bug(String name, String type) {
         this.name = name;
         this.type = type;
         this.items = new ArrayList<String>();
         this.x_position = 0;
         this.y_position = 0;
-        this.size = size;
-        this.alertness = alertness;
-
-
+        this.size = 1;
+        this.energy = 20;
     }
-
     
     void grab(String item) {
-        //checks alertness to see if bu has enough energy to grab
+        if (this.energy <2) {
+            throw new RuntimeException("Sorry, " + this.name +" doesn't have enough energy to grab " + item);
+        }
+        if (this.items.size() ==2) {
+            throw new RuntimeException("Sorry, " + this.name +" cannot hold any more items");
+        }
+        if (this.energy >=2 && this.items.size() < 2){
+            items.add(item);
+            this.energy-=2;
+            System.out.println(this.name + " grabbed " + item);
+        }
+
     }
     String drop(String item) {
-        //drops item if it is holding it 
-        //Certain tye=pes can only hold certain items maybe?
+        if (this.items.size() ==0) {
+            throw new RuntimeException("Sorry, " + this.name +" isn't holding any items");
+        }
+        if (!this.items.contains(item)) {
+            throw new RuntimeException("Sorry, " +this.name + " isn't holding " + item);
+        }
+        else {
+            this.items.remove(item);
+            System.out.println(this.name + " dropped " +item);
+            return item;
+        }
     }
     void examine(String item) {
-        //checks alertness to see if Bug has enough energy to examine
+        if (this.energy <1) {
+            throw new RuntimeException("Sorry, " + this.name +" doesn't have enough energy to examine " + item);
+        }
+        if (this.energy >=1) {
+            System.out.println("...Currently examining "+item+"...");
+            this.energy -=1;
+            System.out.println(this.name +" succesfully examined "+item);
+        }
+
     }
     void use(String item) {
-        //Checks alletness to see if bug has enough energy and checks if bug is holding an object
+        if (this.energy <5) {
+            throw new RuntimeException("Sorry, " + this.name +" doesn't have enough energy to use " + item);
+        }
+        if (this.energy >=5 && !this.items.contains(item)) {
+            this.grab(item);
+            this.energy-=3;
+            System.out.println("...Currently using "+item+"...");
+            this.drop(item);
+            System.out.println(this.name +" succesfully used "+item);
+        }
+        if (this.energy>=5 && this.items.contains(item)) {
+            this.energy-=3;
+            System.out.println("...Currently using "+item+"...");
+            System.out.println(this.name +" succesfully used "+item);
+        }
+
     }
     boolean walk(String direction) {
-        //Check if alertness is high enough move x numbers in specified direction
+        if (this.energy <5) {
+            throw new RuntimeException("Sorry, " + this.name +" doesn't have enough energy to walk");
+        }
+        if (direction!= "North" && direction!="South" && direction!="East" && direction!="West") {
+            throw new RuntimeException("Not a valid direction");
+        }
+        if (this.energy >=5) {
+            if (direction=="North") {
+                System.out.println("...Currently Walking...");
+                this.y_position+=10; 
+                this.energy -=5;
+                System.out.println(this.name + " walked 10 inches " + direction);
+                return true;
+            }
+            if (direction == "South") {
+                System.out.println("...Currently Walking...");
+                this.y_position-=10; 
+                this.energy -=5;
+                System.out.println(this.name + " walked 10 inches " + direction);
+                return true;
+            }
+            if (direction == "East") {
+                System.out.println("...Currently Walking...");
+                this.x_position+=10;
+                this.energy -=5; 
+                System.out.println(this.name + " walked 10 inches " + direction);
+                return true;
+            }
+            if (direction == "West") {
+                System.out.println("...Currently Walking...");
+                this.y_position-=10;
+                this.energy -=5; 
+                System.out.println(this.name + " walked 10 inches " + direction);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+
     }
     boolean fly(int x, int y) {
-        //Check if alertness is high enough increase or decease x and y
+        if (this.energy <10) {
+            throw new RuntimeException("Sorry, " + this.name +" doesn't have enough energy to fly");
+        }
+        if (this.energy >=10) {
+            System.out.println("...Currently Flying...");
+            this.x_position +=x;
+            this.y_position+=y;
+            this.energy-=10; 
+            System.out.println("Flight Complete!");
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     Number shrink() {
-        //decrease size
+        if (this.size ==1) {
+            throw new RuntimeException(this.name + " cannot shrink anymore!");
+        }
+        else {
+            this.size -=1;
+            System.out.println(this.name + " shrunk to size "+ this.size);
+        }
+        return this.size;
     }
     Number grow() {
-        //increase size
+        if (this.size ==10) {
+            throw new RuntimeException(this.name +" cannot grow anymore!");
+        }
+        if (this.energy < 30) {
+            throw new RuntimeException("Sorry, " + this.name +" does not have enough energy to grow");
+        }
+        if (this.energy >= 30) {
+            this.energy -=30;
+            this.size +=1;
+            System.out.println(this.name + " grew to size " + this.size);
+        }
+        return this.size;
     }
     void rest() {
-        //increase alertness if alertness reaches a certain point increase size(grow)
+        this.energy+=10;
+        if (this.energy == 50 || this.energy >50) {
+            this.grow();
+        }
 
     }
     void undo() {
-        //overloaded method
+        this.items.clear();
+        this.x_position=0;
+        this.y_position=0;
+        this.size = 1;
+        this.energy = 20;
+    }
+    public static void main(String[] args) {
+        Bug Egg = new Bug("Egg", "Butterfly");
+        System.out.println(Egg.energy);
+        //Egg.grab("Yoke");
+        //System.out.println(Egg.energy);
+        //Egg.drop("Yoke");
+        //System.out.println(Egg.energy);
+        //Egg.grab("Shell");
+        //Egg.grab("Egg White");
+        //Egg.examine("Yoke");
+        //Egg.use("Yoke");
+        //Egg.walk("North");
+        //Egg.fly(10, 11);
+        //Egg.rest();
+        //System.out.println(Egg.x_position);
+        //System.out.println(Egg.y_position);
+        //Egg.fly(1,2);
+        //Egg.shrink();
+        //System.out.println(Egg.size);
+        //Egg.shrink();
+        //Egg.grow();
+        //Egg.rest();
+        //Egg.rest();
+        //Egg.rest();
+        //System.out.println(Egg.size);
+        //System.out.println(Egg.energy);
+        //Egg.undo();
+        //System.out.println(Egg.size);
+        //System.out.println(Egg.energy);
+        //System.out.println(Egg.x_position);
+        //System.out.println(Egg.y_position);
     }
 }
